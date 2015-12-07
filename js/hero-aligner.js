94a082a76e88492430dc430cc91dbbb67e51ddc4
austinjp <austin.plunkett@gmail.com>
2015-12-07 17:12:23 +0000

    Added stub JS file for hero background image alignment

---
+++ js/hero-aligner.js
@@ -0,0 +1,6 @@
+/**
+   Align the hero element background image by setting the background-position
+   CSS property. Due to Edin theme not being easily "pluggable" it's easiest to do
+   this in JavaScript - although not ideal.
+*/
+
