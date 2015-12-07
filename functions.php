b5c123ff335c4faec0963f2d9fa5cb04049b8d10
austinjp <austin.plunkett@gmail.com>
2015-12-07 14:02:24 +0000

    Thumbnails replaced with background images which are aligned with "image_focus" custom field

--- functions.php
+++ functions.php
@@ -12,7 +12,19 @@ function theme_enqueue_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
+
+/*
+function edin_improved_theme_setup() {
+  add_theme_support( 'post-thumbnails' );
+}
+add_action( 'after_setup_theme', 'edin_improved_theme_setup' );
+*/
+
+
 function edin_improved_post_thumbnail() {
+
+  $postid = get_the_ID();
+
   if ( post_password_required() || is_attachment() || ! has_post_thumbnail() || has_post_format() )
     {
       return;
@@ -22,26 +34,23 @@ function edin_improved_post_thumbnail() {
   case 'square':
     ?><a class="post-thumbnail edin-improved-post-thumbnail-square" <?php
     ?>href="<?php the_permalink(); ?>" <?php
-    ?>style="background-image: url('/wp-content/uploads/2015/12/2-1230x1230.jpg');
-    background-size: cover;
-    -webkit-background-size: cover;
-    -moz-background-size: cover;
-    -o-background-size: cover;
-    background-position: center left;
-    background-repeat: no-repeat;"><?php
     break;
   default:
     ?><a class="post-thumbnail edin-improved-post-thumbnail-landscape" <?php
     ?>href="<?php the_permalink(); ?>" <?php
-    ?>style="background-image: url('/wp-content/uploads/2015/12/2-1230x1230.jpg');
-    background-size: cover;
-    -webkit-background-size: cover;
-    -moz-background-size: cover;
-    -o-background-size: cover;
-    background-position: center left;
-    background-repeat: no-repeat;"><?php
   }
-  ?><img class="edin-improved-post-thumbnail" src="<?php echo '' . get_stylesheet_directory_uri() . '/transparent.png' ?>"><?php
+  ?>style="background-image: url('<?php
+$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $postid ), 'edin-hero' );
+echo esc_url( $thumbnail[0] );
+/* echo '/wp-content/uploads/2015/12/2-1230x1230.jpg'; */
+?>');
+  background-repeat: no-repeat;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-position: <?php echo get_post_meta($postid, 'image_focus', 1); ?> ;">
+  <img class="edin-improved-post-thumbnail" src="<?php echo '' . get_stylesheet_directory_uri() . '/transparent.png' ?>"><?php
   ?></a><?php
 }
 
