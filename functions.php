4918a77c9f9ed229fe81c5074add5cb7f77a852f
austinjp <austin.plunkett@gmail.com>
2015-12-07 15:42:47 +0000

    Use thumbnail instead of transparent, might make for more reliable fall-back display of thumbnails

--- functions.php
+++ functions.php
@@ -13,14 +13,6 @@ function theme_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
 
-/*
-function edin_improved_theme_setup() {
-  add_theme_support( 'post-thumbnails' );
-}
-add_action( 'after_setup_theme', 'edin_improved_theme_setup' );
-*/
-
-
 function edin_improved_post_thumbnail() {
 
   $postid = get_the_ID();
@@ -40,9 +32,8 @@ function edin_improved_post_thumbnail() {
     ?>href="<?php the_permalink(); ?>" <?php
   }
   ?>style="background-image: url('<?php
-$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $postid ), 'edin-hero' );
+$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $postid ), 'edin-thumbnail-landscape' );
 echo esc_url( $thumbnail[0] );
-/* echo '/wp-content/uploads/2015/12/2-1230x1230.jpg'; */
 ?>');
   background-repeat: no-repeat;
   background-size: cover;
@@ -50,28 +41,12 @@ echo esc_url( $thumbnail[0] );
   -moz-background-size: cover;
   -o-background-size: cover;
   background-position: <?php echo get_post_meta($postid, 'image_focus', 1); ?> ;">
-  <img class="edin-improved-post-thumbnail" src="<?php echo '' . get_stylesheet_directory_uri() . '/transparent.png' ?>"><?php
+  <img class="edin-improved-post-thumbnail" src="<?php echo esc_url( $thumbnail[0] ); ?>">
+  <?php
   ?></a><?php
-}
 
-  /**  
-  <a class="post-thumbnail" href="<?php the_permalink(); ?>">
-     <?php
-     if ( is_page_template( 'page-templates/front-page.php' ) || is_page_template( 'page-templates/grid-page.php' ) ) {
-       $ratio = get_theme_mod( 'edin_thumbnail_style' );
-       switch ( $ratio ) {
-       case 'square':
-	 the_post_thumbnail( 'edin-thumbnail-square' );
-	 break;
-       default :
-	 the_post_thumbnail( 'edin-thumbnail-landscape' );
-       }
-     } else {
-       the_post_thumbnail( 'edin-featured-image' );
-     }
-  ?>
-  </a>
-
-<?php
+     /* Instead of using the thumbnail image on top of the background image,
+	could use a transparent PNG instead:
+	<img class="edin-improved-post-thumbnail" src="<?php echo '' . get_stylesheet_directory_uri() . '/transparent.png' ?>">
+     */
 }
-  */
