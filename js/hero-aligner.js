70bb3cf63b2f3ca8286fee70f01d8ff8783af218
austinjp <austin.plunkett@gmail.com>
2015-12-08 11:55:50 +0000

    Reads align vars from query string and creates stylesheet in webpage

--- js/hero-aligner.js
+++ js/hero-aligner.js
@@ -3,4 +3,26 @@
    CSS property. Due to Edin theme not being easily "pluggable" it's easiest to do
    this in JavaScript - although not ideal.
 */
+var path = document.getElementById('hero-aligner').src;
+var align = getQueryVariable('align',path);
+align = align.replace(/[^a-zA-Z0-9]/g,'     ');
+align = align.replace(/\s+/g,' ');
 
+var style = document.createElement("style");
+
+/* FIXME This is a bit ugly but works */
+style.appendChild(document.createTextNode('.hero.with-featured-image { background-position: ' + align + '; }'));
+document.head.appendChild(style);
+
+console.log(style);
+
+function getQueryVariable(variable,path) {
+    var query = path;
+    var vars = query.split("?");
+    for (var i=0; i<vars.length; i++) {
+	var pair = vars[i].split("=");
+	if (pair[0] == variable) {
+	    return pair[1];
+	}
+    }
+}
